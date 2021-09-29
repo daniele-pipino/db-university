6296281c851c6fc364e404e3decc892bc4a5d6c7
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-29 15:00:57 +0200

    exercise 1 complete

--- join_sneak.sql
+++ join_sneak.sql
@@ -1,2 +1,9 @@
 -- selezionare tutti gli studenti iscritti al corso di Laurea in Economia
 
+SELECT `students`.`name`,`students`.`surname`,`degrees`.`name`
+FROM `students`
+join `degrees`
+ON `students`.`id` = `degrees`.`department_id`
+WHERE `degrees`.`name`= 'Corso di Laurea in Economia';
+
+--

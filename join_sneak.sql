958904e5d2c1ee3119587c53e7eedf0814f3dc99
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-29 17:06:56 +0200

    es 5 completo

--- join_sneak.sql
+++ join_sneak.sql
@@ -42,4 +42,16 @@ ON `courses`.`id` = `course_teacher`.`teacher_id`
 JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`;
 
--- 
+-- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica 
+
+SELECT `teachers`.`id`,`departments`.`name`,`teachers`.`name`,`teachers`.`surname`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+ON `courses`.`degree_id` =`degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
